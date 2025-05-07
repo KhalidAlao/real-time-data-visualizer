65835b52eb21d5826781f95212da2595ad881ba8
KhalidAlao <alaokha@gmail.com>
2025-05-07 11:06:50 +0100

    Update DataContext.js

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,2 +1,15 @@
 import {createContext, UseState, UseEffect, useContext} from 'react';
 import axios from 'axios';
+
+// Establish data context
+const DataContext = createContext();
+
+export const DataProvider = ({children}) => {
+    // Inital State for data, time and error
+    const [date, SetDate] = useState([]);
+    const [intervalTime, setIntervalTime] = useState(5000); 
+    const [error, setError] = useState(null);
+
+
+    // CoinCap API
+}
